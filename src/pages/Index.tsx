c861d8681c5cb77847b3ffd52680c6da1535ff0b
ndrepepe <apriyo@gmail.com>
2025-12-14 22:43:29 +0700

    [dyad] Mengubah tata letak kartu di halaman Index menjadi satu kolom vertikal. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,7 +47,7 @@ const Index = () => {
         </h1>
       </StickyHeader>
       
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 gap-6">
         <Card>
           <CardHeader>
             <CardTitle>{t('your_current_role')}</CardTitle>
